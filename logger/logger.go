fe290a782bd07bc2656306b20b15a91d5ec25112
Ankur Upadhyaya <ankur@opennetworking.org>
2022-02-03 15:27:23 +0530

    Removed Member-only license

--- logger/logger.go
+++ logger/logger.go
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2021 Open Networking Foundation <info@opennetworking.org>
 //
 // SPDX-License-Identifier: Apache-2.0
-// SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
+//
 
 package logger
 
@@ -10,7 +10,6 @@ import (
 
 	formatter "github.com/antonfisher/nested-logrus-formatter"
 	"github.com/sirupsen/logrus"
-
 )
 
 var (
@@ -38,7 +37,6 @@ func init() {
 		HideKeys:        true,
 		FieldsOrder:     []string{"component", "category"},
 	}
-
 
 	AppLog = log.WithFields(logrus.Fields{"component": "UDR", "category": "App"})
 	InitLog = log.WithFields(logrus.Fields{"component": "UDR", "category": "Init"})
