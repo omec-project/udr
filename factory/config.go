5e18b86d5165be8f27432c6f26e5f66553c1da03
Patricia Reinoso <patricia.reinoso@canonical.com>
2024-01-25 11:06:23 +0100

    Update factory/config.go
    Co-authored-by: gab-arrobo <gabriel.arrobo@intel.com>

--- factory/config.go
+++ factory/config.go
@@ -54,7 +54,6 @@ type Sbi struct {
 	Tls          *Tls   `yaml:"tls,omitempty"`
 	Scheme       string `yaml:"scheme"`
 	RegisterIPv4 string `yaml:"registerIPv4,omitempty"` // IP that is registered at NRF.
-	// IPv6Addr string `yaml:"ipv6Addr,omitempty"`
 	BindingIPv4 string `yaml:"bindingIPv4,omitempty"` // IP used to run the server in the node.
 	Port        int    `yaml:"port"`
 }
