9e22362cae5f101a84107b9eca3d2bc1cba3ed56
gab-arrobo <gabriel.arrobo@intel.com>
2024-10-22 12:51:52 -0700

    Clean up logging and remove unused file (#136)
    * Clean up logging
    
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>
    
    * Remove unused file
    
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>
    
    * Update golangci file
    
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>
    
    ---------
    
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>

--- udr.go
+++ udr.go
@@ -12,26 +12,19 @@ import (
 	"github.com/omec-project/udr/logger"
 	udr_service "github.com/omec-project/udr/service"
 	"github.com/urfave/cli"
-	"go.uber.org/zap"
 )
 
 var UDR = &udr_service.UDR{}
 
-var appLog *zap.SugaredLogger
-
-func init() {
-	appLog = logger.AppLog
-}
-
 func main() {
 	app := cli.NewApp()
 	app.Name = "udr"
-	appLog.Infoln(app.Name)
+	logger.AppLog.Infoln(app.Name)
 	app.Usage = "-free5gccfg common configuration file -udrcfg udr configuration file"
 	app.Action = action
 	app.Flags = UDR.GetCliCmd()
 	if err := app.Run(os.Args); err != nil {
-		appLog.Errorf("UDR Run error: %v", err)
+		logger.AppLog.Errorf("UDR Run error: %v", err)
 	}
 }
 
