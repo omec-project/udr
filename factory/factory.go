66c7cc2fed98d95c7e1c3d3d10c0e7c8125d61f2
gatici <gulsum.atici@canonical.com>
2024-10-23 15:30:45 +0300

    chore: change the orders
    Signed-off-by: gatici <gulsum.atici@canonical.com>

--- factory/factory.go
+++ factory/factory.go
@@ -58,7 +58,6 @@ func InitConfigFactory(f string) error {
 		}
 		if os.Getenv("MANAGED_BY_CONFIG_POD") == "true" {
 			logger.InitLog.Infoln("MANAGED_BY_CONFIG_POD is true")
-
 		} else {
 			go func() {
 				logger.InitLog.Infoln("use helm chart config")
