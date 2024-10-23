9b16a51aeff6c9b6bee181b6a34246f603f10395
gatici <gulsum.atici@canonical.com>
2024-10-23 14:29:23 +0300

    adding gClient
    Signed-off-by: gatici <gulsum.atici@canonical.com>

--- factory/factory.go
+++ factory/factory.go
@@ -58,6 +58,7 @@ func InitConfigFactory(f string) error {
 		}
 		if os.Getenv("MANAGED_BY_CONFIG_POD") == "true" {
 			logger.InitLog.Infoln("MANAGED_BY_CONFIG_POD is true")
+
 		} else {
 			go func() {
 				logger.InitLog.Infoln("use helm chart config")
