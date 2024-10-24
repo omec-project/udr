8f9099d2bf9c5c1264a2f26bee338790b7b420b3
gatici <gulsum.atici@canonical.com>
2024-10-25 01:47:31 +0300

    create stream
    Signed-off-by: gatici <gulsum.atici@canonical.com>

--- service/init.go
+++ service/init.go
@@ -122,9 +122,9 @@ func manageGrpcClient(webuiUri string) {
 				client = nil
 				continue
 			}
-			if stream == nil {
-				stream, _ = client.CheckGrpcConnectivity()
-			}
+
+			stream, _ = client.CheckGrpcConnectivity()
+
 			if configChannel == nil {
 				configChannel = client.PublishOnConfigChange(true, stream)
 				initLog.Infoln("PublishOnConfigChange is triggered.")
