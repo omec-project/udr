61fab7463bcb975f55db3fce12734a3be26f9d60
Arrobo, Gabriel <gabriel.arrobo@intel.com>
2025-02-28 11:02:47 -0800

    Fix/Avoid 100% CPU utilization
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/google/uuid v1.6.0
 	github.com/mitchellh/mapstructure v1.5.0
-	github.com/omec-project/config5g v1.5.7
+	github.com/omec-project/config5g v1.5.8
 	github.com/omec-project/openapi v1.3.3
 	github.com/omec-project/util v1.2.13
 	github.com/prometheus/client_golang v1.21.0
--- service/init.go
+++ service/init.go
@@ -110,7 +110,7 @@ func manageGrpcClient(webuiUri string) {
 	count := 0
 	for {
 		if client != nil {
-			if client.CheckGrpcConnectivity() != "ready" {
+			if client.CheckGrpcConnectivity() != "READY" {
 				time.Sleep(time.Second * 30)
 				count++
 				if count > 5 {
@@ -139,6 +139,8 @@ func manageGrpcClient(webuiUri string) {
 				go factory.UdrConfig.UpdateConfig(configChannel, factory.ConfigUpdateDbTrigger)
 				logger.InitLog.Infoln("UDR updateConfig is triggered")
 			}
+
+			time.Sleep(time.Second * 5) // Fixes (avoids) 100% CPU utilization
 		} else {
 			client, err = grpcClient.ConnectToConfigServer(webuiUri)
 			stream = nil
